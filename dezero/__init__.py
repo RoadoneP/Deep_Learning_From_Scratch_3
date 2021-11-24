f1259d1c97bf9a0c58c7e7d0d472e3c041da5638
Road.1 <a01152a@gmail.com>
2021-11-25 04:29:39 +0900

    50

--- dezero/__init__.py
+++ dezero/__init__.py
@@ -1,6 +1,6 @@
 # =============================================================================
 # step23.py부터 step32.py까지는 simple_core를 이용해야 합니다.
-is_simple_core = True  # True
+is_simple_core = False  # True
 # =============================================================================
 
 if is_simple_core:
@@ -14,15 +14,16 @@ if is_simple_core:
 
 else:
     from dezero.core import Variable
-    from dezero.core import Parameter
+    # from dezero.core import Parameter
     from dezero.core import Function
     from dezero.core import using_config
     from dezero.core import no_grad
-    from dezero.core import test_mode
+    # from dezero.core import test_mode
     from dezero.core import as_array
     from dezero.core import as_variable
     from dezero.core import setup_variable
     from dezero.core import Config
+    '''
     from dezero.layers import Layer
     from dezero.models import Model
     from dezero.datasets import Dataset
@@ -38,6 +39,6 @@ else:
     import dezero.utils
     import dezero.cuda
     import dezero.transforms
-
+    '''
 setup_variable()
 __version__ = '0.0.13'
